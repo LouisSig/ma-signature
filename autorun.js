2c7e5709ff655bf3352db1d1b86dea0b99ac32f9
lverbrugge <lverbrugge@MSI3>
2025-12-18 11:19:30 +0100

    signature

--- autorun.js
+++ autorun.js
@@ -1,25 +1,17 @@
 /* global Office */
 
-
 Office.onReady(() => {
-  // Associe l’ID du manifest à la fonction JS
   Office.actions.associate("onNewMessageComposeHandler", onNewMessageComposeHandler);
-  Office.actions.associate("onNewAppointmentComposeHandler", onNewAppointmentComposeHandler);
 });
 
-function buildSignatureHtml() {
-  return `
+function onNewMessageComposeHandler(event) {
+  const html = `
     <div style="font-family: Calibri, sans-serif; margin-bottom: 12px;">
       <p>Cordialement,</p>
       <strong style="color:#005a9e; font-size:14pt;">Louis Verbrugge</strong><br/>
       <span style="color:#666;">SIGMA France</span><br/>
       <a href="https://www.sigma-france.fr" style="color:#005a9e;">www.sigma-france.fr</a>
-    </div>
-  `;
-}
-
-function setSig(event) {
-  const html = buildSignatureHtml();
+    </div>`;
 
   Office.context.mailbox.item.body.setSignatureAsync(
     html,
@@ -27,12 +19,3 @@ function setSig(event) {
     () => event.completed()
   );
 }
-
-function onNewMessageComposeHandler(event) {
-  setSig(event);
-}
-
-function onNewAppointmentComposeHandler(event) {
-  setSig(event);
-}
-
